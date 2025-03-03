0be940fab32d8c8c19afde4c6a2143f0b3c7274a
H.M Nadun Tilan Weerasinghe <74712552+NADUN-TILAN@users.noreply.github.com>
2025-03-03 17:00:10 +0530

    Navbar design vs.1.0.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import UserViewModel from "./viewmodels/UserViewModel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -10,7 +11,7 @@ import UserList from "./components/UserList";
 import AddUserForm from "./components/AddUserForm";
 import AssignTasks from "./components/AssignTasks";
 import TaskList from "./components/TaskList";
-import Footer from "./components/Footer"; 
+import Footer from "./components/Footer";
 
 const App = () => {
   const { handleusers, handleAddUser, handleAssignTasks, handleDashboard, handleassignTasksMonitoring } = UserViewModel();
@@ -18,24 +19,41 @@ const App = () => {
   return (
     <Router>
       <div>
-        {/* Navigation Links */}
-        <nav>
-          <ul>
-            <li><Link to="/DashBoard">Dashboard</Link></li>
-            <li><Link to="/assign-task">Task Assign</Link></li>
-            <li><Link to="/add-user">Add User</Link></li>
-            <li><Link to="/user-list">View User </Link></li>
-            <li><Link to="/add-tasks">Add Task</Link></li>
-            <li><Link to="/tasks">View Task</Link></li>            
-          </ul>
-        </nav>
+        {/* Navigation Bar */}
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/DashBoard">Task Manager</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/DashBoard">Dashboard</Nav.Link>
+                <Nav.Link as={Link} to="/assign-task">Assign Task</Nav.Link>
+                {/* <Nav.Link as={Link} to="/user-list">View User</Nav.Link> */}
+
+                {/* Dropdown 1 for Task Management */}
+                <NavDropdown title="Users" id="task-dropdown">
+                  <NavDropdown.Item as={Link} to="/add-user">Add User</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/user-list">View User</NavDropdown.Item>
+                  {/* <NavDropdown.Item as={Link} to="/tasks">View Task</NavDropdown.Item> */}
+                </NavDropdown>
+
+                {/* Dropdown 2 for Task Management */}
+                <NavDropdown title="Tasks" id="task-dropdown">
+                  {/* <NavDropdown.Item as={Link} to="/assign-task">Assign Task</NavDropdown.Item> */}
+                  <NavDropdown.Item as={Link} to="/add-tasks">Add Task</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/tasks">View Task</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
         {/* Routes */}
         <Routes>
           <Route path="/DashBoard" element={<DashBoard DashBoard={handleDashboard} />} />
-          <Route path="/assign-task" element={<AssignTasksMonitoring assignTasksMonitoring={handleassignTasksMonitoring} />} /> 
-          <Route path="/user-list" element={<UserList users={handleusers} />} /> 
-          <Route path="/add-user" element={<AddUserForm onAddUser={handleAddUser} />} />         
+          <Route path="/assign-task" element={<AssignTasksMonitoring assignTasksMonitoring={handleassignTasksMonitoring} />} />
+          <Route path="/user-list" element={<UserList users={handleusers} />} />
+          <Route path="/add-user" element={<AddUserForm onAddUser={handleAddUser} />} />
           <Route path="/add-tasks" element={<AssignTasks assignTasks={handleAssignTasks} />} />
           <Route path="/tasks" element={<TaskList />} />
         </Routes>
@@ -43,6 +61,7 @@ const App = () => {
 
       {/* Footer */}
       <Footer />
+
     </Router>
   );
 };
